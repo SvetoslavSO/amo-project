851bd4ee220da26bd3cbda5bd5df8f4a57525a15
SvetoslavSO <sshaunin@mail.ru>
2023-05-03 12:17:25 +0300

    commit amo-project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,61 +11,65 @@ export const Header = () => {
       <div className="container">
         <header className="header">
           <div className="row">
-            <div className="logo">
-              <img className="logo__picture" src={logo} alt="" />
+            <div className="row__left">
+              <div className="logo">
+                <img className="logo__picture" src={logo} alt="" />
+              </div>
+              <nav className="navigation desktop">
+                <ul className="navigation__list">
+                  <li className="navigation__item">
+                    Услуги
+                  </li>
+                  <li className="navigation__item">
+                    Виджеты
+                  </li>
+                  <li className="navigation__item">
+                    Интеграции
+                  </li>
+                  <li className="navigation__item">
+                    Кейсы
+                  </li>
+                  <li className="navigation__item">
+                    Сертификаты
+                  </li>
+                </ul>
+              </nav>
             </div>
-            <nav className="navigation desktop">
-              <ul className="navigation__list">
-                <li className="navigation__item">
-                  Услуги
-                </li>
-                <li className="navigation__item">
-                  Виджеты
-                </li>
-                <li className="navigation__item">
-                  Интеграции
-                </li>
-                <li className="navigation__item">
-                  Кейсы
-                </li>
-                <li className="navigation__item">
-                  Сертификаты
-                </li>
-              </ul>
-            </nav>
-            <nav className="navigation mobile">
-              <ul className="navigation__list">
-                <li className="navigation__item">
-                  Услуги
-                </li>
-                <li className="navigation__item">
-                  Виджеты
-                </li>
-                <li className="navigation__item">
-                  Интеграции
-                </li>
-                <li className="navigation__item">
-                  Кейсы
-                </li>
-              </ul>
-            </nav>
-            <div className='contacts__header'>
-              <a className='contacts__link' href="tel:+75555555555">
-                +7 555 555-55-55
-              </a>
-            </div>
-            <div className='messengers'>
-              <ul className='messengers__list'>
-                <li className='messengers__item'>
-                  <img className='messengers__picture' src={telegram} alt="" />
-                </li>
-                <li className='messengers__item'>
-                  <img className='messengers__picture' src={viber} alt="" />
-                </li>
-                <li className='messengers__item'>
-                  <img className='messengers__picture' src={whatsApp} alt="" />
-                </li>
-              </ul>
+            <div className="row__right">
+              <nav className="navigation mobile">
+                <ul className="navigation__list">
+                  <li className="navigation__item">
+                    Услуги
+                  </li>
+                  <li className="navigation__item">
+                    Виджеты
+                  </li>
+                  <li className="navigation__item">
+                    Интеграции
+                  </li>
+                  <li className="navigation__item">
+                    Кейсы
+                  </li>
+                </ul>
+              </nav>
+              <div className='contacts__header'>
+                <a className='contacts__link' href="tel:+75555555555">
+                  +7 555 555-55-55
+                </a>
+              </div>
+              <div className='messengers'>
+                <ul className='messengers__list'>
+                  <li className='messengers__item'>
+                    <img className='messengers__picture' src={telegram} alt="" />
+                  </li>
+                  <li className='messengers__item'>
+                    <img className='messengers__picture' src={viber} alt="" />
+                  </li>
+                  <li className='messengers__item'>
+                    <img className='messengers__picture' src={whatsApp} alt="" />
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
           <div className='row mobile'>
